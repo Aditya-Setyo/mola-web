01863c7e3ca4e11066946459c75c34a6cdb264a9
Aditya-Setyo <gataco05377@gmail.com>
2025-07-19 10:26:34 +0700

    Update TopSell

--- frontend/src/components/topsell.jsx
+++ frontend/src/components/topsell.jsx
@@ -26,7 +26,6 @@ const TopSell = () => {
             product.category_name ||
             product.category?.name ||
             (typeof product.category === "string" ? product.category : "");
-
           return categoryName?.toLowerCase().includes("penjualan teratas");
         });
 
@@ -51,28 +50,34 @@ const TopSell = () => {
   return (
     <section className="px-10 py-16">
       <h2 className="text-2xl font-bold mb-6 text-center">PENJUALAN TERATAS</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
         {products.length > 0 ? (
           products.map((product) => (
-            <Link to={`/productdetails/${product.id}`}>
-              <div
-                key={product.id}
-                className="w-full max-w-xs bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden flex flex-col justify-between transition-transform hover:scale-105"
-              >
+            <div
+              key={product.id}
+              className="bg-white border border-gray-200 rounded-2xl shadow hover:shadow-md transition duration-300 hover:scale-105 overflow-hidden"
+            >
+              <Link to={`/productdetails/${product.id}`}>
                 <img
-                  src={`http://molla.my.id${product.image_url}`}
+                  src={`https://molla.my.id${product.image_url}`}
                   alt={product.name}
-                  className="w-full h-48 object-contain bg-white"
+                  className="w-full h-48 object-contain bg-white p-4"
                 />
-                <div className="p-4 text-center">
-                  <h3 className="text-lg font-semibold text-gray-800 truncate">{product.name}</h3>
-                  <p className="text-sm text-gray-600 mt-1">Rp {product.price.toLocaleString()}</p>
+                <div className="px-4 pb-4 text-center">
+                  <h3 className="text-base font-semibold text-gray-800 truncate">
+                    {product.name}
+                  </h3>
+                  <p className="text-sm text-gray-600 mt-1">
+                    Rp {product.price.toLocaleString()}
+                  </p>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           ))
         ) : (
-          <p className="text-center col-span-4 text-gray-500">Tidak ada produk terbaru</p>
+          <p className="text-center col-span-2 md:col-span-4 text-gray-500">
+            Tidak ada produk terbaru
+          </p>
         )}
       </div>
     </section >
